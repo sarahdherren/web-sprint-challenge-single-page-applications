abe473c7b5e5b871b45f57e5a2d1c12bcc1aa538
Sarah D Herren <herren.sarah@icloud.com>
2021-06-12 23:55:37 -0500

    removed bootstrap styling to hopefully get code grade to find my link & route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Nav, NavItem, Navbar } from "react-bootstrap";
 import { LinkContainer } from 'react-router-bootstrap';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import API_URL from './constants/urls';
 import PizzaForm from './components/PizzaForm';
 import Home from './components/Home';
@@ -103,9 +103,7 @@ const App = () => {
         </Navbar.Brand>
         <Navbar.Collapse className='justify-content-end'>
         <NavItem>
-          <LinkContainer id='order-pizza' to='/pizza'>
-            <Nav.Link >Order</Nav.Link>
-          </LinkContainer>
+          <Link id='order-pizza' to='/pizza'>Order</Link>
         </NavItem>
         {/* <NavItem>
           <LinkContainer to='/'> 
