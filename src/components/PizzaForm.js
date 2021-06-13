efbbc4252aba64a0a26b4bcecf19cf3e143efe14
Sarah D Herren <herren.sarah@icloud.com>
2021-06-12 22:40:32 -0500

    removed errornoues import line

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap'
-import InputGroupWithExtras from 'react-bootstrap/esm/InputGroup';
 
 const imageHero = require('../designFiles/pizzaPie.jpg');
 
