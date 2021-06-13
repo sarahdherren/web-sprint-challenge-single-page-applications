78ead35c5ca45b1246a2869c398b4574def9fec6
Sarah D Herren <herren.sarah@icloud.com>
2021-06-13 11:55:44 -0500

    cypress  testing file implemented cypress testing passed!

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -1,10 +1,37 @@
 describe('Pizza Form', () => {
 
     beforeEach(() => {
-        cy.visit('http://localhose:3000')
+        cy.visit('http://localhost:3000/pizza')
     })
 
     it('sanity checks', () => {
         expect(5).to.equal(5)
     })
+
+    const name = () => cy.get("#name-input")
+    const checkboxes = () => cy.get('.toppings')
+    const submit = () => cy.get('#order-button')
+    const select = () => cy.get('#size-dropdown')
+    
+    it('the proper elements exist', () => {
+        name().should('exist')
+        checkboxes().should('exist')
+        submit().should('exist')
+    })
+
+    it('can enter a name into the text input', () => {
+        name()
+            .type('Kenneth')
+            .should('have.value', 'Kenneth')
+    })
+
+    it('can check multiple topping options', () => {
+        checkboxes().check
+    })
+
+    it('can submit form', () => {
+        name().type('Kenneth')
+        submit().click
+    })
+
 })
